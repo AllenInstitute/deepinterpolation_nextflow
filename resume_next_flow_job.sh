647fdfa04a65e5b1e96e1a48a37fa55749fd0600
Jerome Lecoq <jlecoq@gmail.com>
2023-02-15 15:58:39 -0800

    add resume function

---
+++ resume_next_flow_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=4GB
+#SBATCH --partition=braintv
+#SBATCH --time=24:00:00
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate /allen/programs/mindscope/workgroups/openscope/environment/nextflow
+cd /home/jeromel/deepinterpolation_deploy/deepinterpolation_nextflow
+./nextflow deepinterpolation.nf -resume
